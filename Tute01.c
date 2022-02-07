b2e9fbb65f2624efcf23687f44b1e794f30fe3e0
IT21349010 <99186495+IT21349010@users.noreply.github.com>
2022-02-07 21:57:14 +0530

    Update Tute01.c

--- Tute01.c
+++ Tute01.c
@@ -5,6 +5,16 @@
 #include <stdio.h>
 
 int main() {
+   int no1,no2;
+	float average;
+	
+	printf("Enter the mark of module: ");
+	scanf("%d", &no1);
+	printf("Enter the mark of module: ");
+	scanf("%d", &no2);
+	
+	average = (no1 + no2) / 2;
+	printf("Average is %.2f\n",average);
   
   return 0;
 }
